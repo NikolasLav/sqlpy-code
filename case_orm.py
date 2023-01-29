685ae3fd88a0db90de51fdc21a59bae184fff1fc
NikolasLav <mp4ster@gmail.com>
2023-01-29 20:00:46 +0300

    Update case_orm.py

--- case_orm.py
+++ case_orm.py
@@ -47,7 +47,7 @@ class Sale(Base):
 	__tablename__ = "sale"
 	id = sq.Column(sq.Integer, primary_key=True)
 	price = sq.Column(sq.Float, nullable=False)
-	datesale = sq.Column(sq.Date, nullable=False)
+	date_sale = sq.Column(sq.Date, nullable=False)
 	id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
 	count = sq.Column(sq.Integer, nullable=False)
 	stock = relationship(Stock, backref="sale")
@@ -91,53 +91,61 @@ def add_sale(id, price, date_sale, count, id_stock):
 def take_data():
 	with open('fixtures/tests_data.json', encoding='utf-8') as new_file:
 		data = json.load(new_file)
-	for item in data:
-		if item['model'] == 'publisher':
-			try:
-				add_publisher(item['pk'], item['fields']['name'])
-			except:
-				print('ошибка с добавлением объекта:', item)
-		if item['model'] == 'shop':
-			try:
-				add_shop(item['pk'], item['fields']['name'])
-			except:
-				print('ошибка с добавлением объекта:', item)
-		if item['model'] == 'book':
-			try:
-				add_book(item['pk'], item['fields']['title'], item['fields']['id_publisher'])
-			except:
-				print('ошибка с добавлением объекта:', item)
-		if item['model'] == 'stock':
-			try:
-				add_stock(item['pk'], item['fields']['id_shop'], item['fields']['id_book'], item['fields']['count'])
-			except:
-				print('ошибка с добавлением объекта:', item)
-		if item['model'] == 'sale':
-			try:
-				add_sale(item['pk'], item['fields']['price'], item['fields']['date_sale'], item['fields']['count'], item['fields']['id_stock'])
-			except:
-				print('ошибка с добавлением объекта:', item)
+	for record in data:
+		model = {
+			'publisher': Publisher,
+			'shop': Shop,
+			'book': Book,
+			'stock': Stock,
+			'sale': Sale,
+		}[record.get('model')]
+		print(model(id=record.get('pk'), **record.get('fields')))
+		# if item['model'] == 'publisher':
+		# 	try:
+		# 		add_publisher(item['pk'], item['fields']['name'])
+		# 	except:
+		# 		print('ошибка с добавлением объекта:', item)
+		# if item['model'] == 'shop':
+		# 	try:
+		# 		add_shop(item['pk'], item['fields']['name'])
+		# 	except:
+		# 		print('ошибка с добавлением объекта:', item)
+		# if item['model'] == 'book':
+		# 	try:
+		# 		add_book(item['pk'], item['fields']['title'], item['fields']['id_publisher'])
+		# 	except:
+		# 		print('ошибка с добавлением объекта:', item)
+		# if item['model'] == 'stock':
+		# 	try:
+		# 		add_stock(item['pk'], item['fields']['id_shop'], item['fields']['id_book'], item['fields']['count'])
+		# 	except:
+		# 		print('ошибка с добавлением объекта:', item)
+		# if item['model'] == 'sale':
+		# 	try:
+		# 		add_sale(item['pk'], item['fields']['price'], item['fields']['date_sale'], item['fields']['count'], item['fields']['id_stock'])
+		# 	except:
+		# 		print('ошибка с добавлением объекта:', item)
 
 
-def find_shop_by_publisher(publisher = input('Введите id или название издателя: ')):
-	try:
-		publisher=int(publisher)
-		q = session.query(Shop.name).distinct(Shop.name).join(Stock).join(Book).filter(Book.id_publisher == publisher)
+# def find_shop_by_publisher(publisher = input('Введите id или название издателя: ')):
+# 	try:
+# 		publisher=int(publisher)
+# 		q = session.query(Shop.name).distinct(Shop.name).join(Stock).join(Book).filter(Book.id_publisher == publisher)
 
-	except:
-		q = session.query(Shop.name).distinct(Shop.name).join(Stock).join(Book).join(Publisher).filter(Publisher.name == publisher)
+# 	except:
+# 		q = session.query(Shop.name).distinct(Shop.name).join(Stock).join(Book).join(Publisher).filter(Publisher.name == publisher)
 
-	print('Магазины, в которых представлен издатель:')
-	for s in q.all():
-		print(*s)
+# 	print('Магазины, в которых представлен издатель:')
+# 	for s in q.all():
+# 		print(*s)
 
 
-server, password, uri, port, db_name = db.settings.values()
-DSN = f"postgresql://{server}:{password}@{uri}:{port}/{db_name}"
-engine = sq.create_engine(DSN, echo=None)
-Session = sessionmaker(bind=engine)
-session = Session()
+# server, password, uri, port, db_name = db.settings.values()
+# DSN = f"postgresql://{server}:{password}@{uri}:{port}/{db_name}"
+# engine = sq.create_engine(DSN, echo=None)
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
-create_tables(engine) #создаем таблицы в БД
+# create_tables(engine) #создаем таблицы в БД
 take_data() #импортируем данные в БД из файла по заданию
-find_shop_by_publisher() # Выводит название магазинов (shop), в которых представлены книги конкретного издателя, получая имя или идентификатор издателя (publisher)
+# find_shop_by_publisher() # Выводит название магазинов (shop), в которых представлены книги конкретного издателя, получая имя или идентификатор издателя (publisher)
